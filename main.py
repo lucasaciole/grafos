3e0466750eaf6c53a65120b96aa3bb314fdd3436
Lucas Yuji Suguinoshita Aciole <lucas.aciole@gmail.com>
2017-02-14 21:29:10 -0200

    Add missing import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 import json
 app = Flask(__name__)
 app.config["APPLICATION_ROOT"] = "/areadetrabalho"
